037084baa2f727b39d9e60d6c87f703b245e14c0
rakan <Rakan.w10@outlook.com>
2023-12-01 17:41:04 +0300

    Initial Commit

---
+++ Sehil/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Sehil
+//
+//  Created by Rakan Alotaibi on 17/05/1445 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Sehil/SehilApp.swift
@@ -0,0 +1,17 @@
+//
+//  SehilApp.swift
+//  Sehil
+//
+//  Created by Rakan Alotaibi on 17/05/1445 AH.
+//
+
+import SwiftUI
+
+@main
+struct SehilApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
